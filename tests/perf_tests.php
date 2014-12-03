15af5f40cabba00284d4cfe3a04ecde9cecb17b4
Prateek Agarwal <prat0318@gmail.com>
2014-12-03 14:00:44 -0600

    Add tests changes

--- tests/perf_tests.php
+++ tests/perf_tests.php
@@ -1,8 +1,10 @@
 <?
-$navphp_old = "dcac";
+// Usage: php perf_tests.php old/new login/ls/cat/create/shared
+$navphp_old = "dcac:5566";
 $navphp_new = "dcac:5555";
 
 $arg = $argv[1];
+$test = $argv[2];
 $url = ($arg == "old" ? $navphp_old : $navphp_new);
 
 $ch = curl_init();
@@ -15,42 +17,95 @@ curl_setopt($ch, CURLOPT_COOKIESESSION, true);
 curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie-name');  //could be empty, but cause problems on some hosts
 curl_setopt($ch, CURLOPT_COOKIEFILE, 'tmp0');  //could be empty, but cause problems on some hosts
 $answer = curl_exec($ch);
-echo "completed the post call... $answer";
+//echo "completed the post call... $answer";
 if (curl_error($ch)) {
     echo curl_error($ch);
     }
-echo "------";
-// LOGIN:
-// curl_setopt($ch, CURLOPT_URL, "http://$url/navphp/windows.php");
-//curl_setopt($ch, CURLOPT_POST, false);
-//curl_setopt($ch, CURLOPT_POSTFIELDS, "");
-// CAT:
-// curl_setopt($ch, CURLOPT_URL, "http://$url/navphp/windows.php?action=Open&dir=L2hvbWUvdXNlcjQ=&file=bmV3X2ZpbGUudHh0");
-//curl_setopt($ch, CURLOPT_POST, false);
-//curl_setopt($ch, CURLOPT_POSTFIELDS, "");
-// LS with 100 files present:
-//curl_setopt($ch, CURLOPT_URL, "http://$url/navphp/windows.php");
-//curl_setopt($ch, CURLOPT_POST, false);
-//curl_setopt($ch, CURLOPT_POSTFIELDS, "");
-// CREAT empty files
-//curl_setopt($ch, CURLOPT_URL, "http://$url/navphp/newfile.php");
-//curl_setopt($ch, CURLOPT_POSTFIELDS, "dir=L2hvbWUvdXNlcjQ=&ajax=1&change=".base64_encode("createn_$i.txt"));
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// OPEN SHARED FILE
-//curl_setopt($ch, CURLOPT_URL, "http://$url/navphp/windows.php?action=Open&dir=L2hvbWUvdXNlcjMvbmV3X2ZvbGRlcg==&file=bmV3X2ZpbGUudHh0");
-//curl_setopt($ch, CURLOPT_POST, false);
-//curl_setopt($ch, CURLOPT_POSTFIELDS, "");
 
-$time = microtime(true); // time in Microseconds
-
-for ($i = 1; $i <= 10; $i++) {
-    //curl_setopt($ch, CURLOPT_POSTFIELDS, "dir=L2hvbWUvdXNlcjQ=&ajax=1&change=".base64_encode("createm_$i.txt"));
-    $answer = curl_exec($ch);
-    if (curl_error($ch)) {
-        echo curl_error($ch);
+$sum = 0;
+switch($test) {
+case "login":
+    // LOGIN:
+    curl_setopt($ch, CURLOPT_URL, "http://$url/navphp/windows.php");
+    curl_setopt($ch, CURLOPT_POST, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "");
+    for ($j = 1; $j <= 50; $j++) {
+        $time = microtime(true); // time in Microseconds
+        for ($i = 1; $i <= 10; $i++) {
+            //curl_setopt($ch, CURLOPT_POSTFIELDS, "dir=L2hvbWUvdXNlcjQ=&ajax=1&change=".base64_encode("createm_$i.txt"));
+            $answer = curl_exec($ch);
+            if (curl_error($ch)) {
+                echo curl_error($ch);
+                }
         }
+    //echo "completed the get call... $answer";
+    echo (microtime(true) - $time) . ' elapsed \n'. $arg;
+    $sum += microtime(true) - $time;
+    }
+    break;
+case "cat":
+    // CAT:
+    curl_setopt($ch, CURLOPT_URL, "http://$url/navphp/windows.php?action=Open&dir=L2hvbWUvdXNlcjQ=&file=bmV3X2ZpbGUudHh0");
+    curl_setopt($ch, CURLOPT_POST, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "");
+    for ($j = 1; $j <= 50; $j++) {
+        $time = microtime(true); // time in Microseconds
+        for ($i = 1; $i <= 10; $i++) {
+            $answer = curl_exec($ch);
+            if($j == 1 && $i == 1) echo "completed the get call... $answer";
+        }
+        echo (microtime(true) - $time) . ' elapsed \r\n'. $arg;
+    $sum += microtime(true) - $time;
+    }
+    break;
+case "create":
+    // CREATE:
+    curl_setopt($ch, CURLOPT_URL, "http://$url/navphp/newfile.php");
+    curl_setopt($ch, CURLOPT_POST, true);
+    //curl_setopt($ch, CURLOPT_POSTFIELDS, "dir=L2hvbWUvdXNlcjQ=&ajax=1&change=".base64_encode("createn$j_$i.txt"));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    for ($j = 1; $j <= 50; $j++) {
+        $time = microtime(true); // time in Microseconds
+        for ($i = 1; $i <= 10; $i++) {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, "dir=L2hvbWUvdXNlcjQ=&ajax=1&change=".base64_encode("create_$j_$i.txt"));
+            $answer = curl_exec($ch);
+            //if($j == 1 && $i == 1) echo "completed the get call... $answer";
+        }
+        echo (microtime(true) - $time) . " elapsed \r\n". $arg;
+    $sum += microtime(true) - $time;
+    }
+    break;
+case "ls":
+    // LS with 100 files present:
+    curl_setopt($ch, CURLOPT_URL, "http://$url/navphp/windows.php");
+    curl_setopt($ch, CURLOPT_POST, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "");
+    for ($j = 1; $j <= 50; $j++) {
+        $time = microtime(true); // time in Microseconds
+        for ($i = 1; $i <= 10; $i++) {
+            $answer = curl_exec($ch);
+        }
+        echo (microtime(true) - $time) . " elapsed \r\n". $arg;
+    $sum += microtime(true) - $time;
+    }
+    break;
+case "shared":
+    // OPEN SHARED FILE
+    curl_setopt($ch, CURLOPT_URL, "http://$url/navphp/windows.php?action=Open&dir=L2hvbWUvdXNlcjMvbmV3X2ZvbGRlcg==&file=bmV3X2ZpbGUudHh0");
+    curl_setopt($ch, CURLOPT_POST, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "");
+    for ($j = 1; $j <= 50; $j++) {
+        $time = microtime(true); // time in Microseconds
+        for ($i = 1; $i <= 10; $i++) {
+            $answer = curl_exec($ch);
+        }
+        echo (microtime(true) - $time) . " elapsed \r\n". $arg;
+    $sum += microtime(true) - $time;
+    }
+    break;
 }
-echo "completed the get call... $answer";
-echo (microtime(true) - $time) . ' elapsed '. $arg;
+
+echo ("Average: ".($sum/50)."sum: $sum");
+
 ?>
 
